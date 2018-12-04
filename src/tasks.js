f4781ecd9b2ab83e469124928f6f42fa7624ad23
stevedesilva <steve.desilva@gmail.com>
2018-12-04 07:45:33 +0000

    add reducer

--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,51 @@
 import { generate as id} from 'shortid';
-import { Dispatcher, ReduceStore } from './flux'
+import { Dispatcher, ReduceStore } from './flux';
 
 const tasksDispatcher = new Dispatcher();
+
+class TasksStore extends ReduceStore {
+  getInitialState() {
+    return {
+      tasks : [{
+        id: id(),
+        content: 'Do NGN Migration',
+        complete: 'true'
+      },
+      {
+        id: id(),
+        content: 'Update CAPI code for route to SPOT and Livefyre',
+        complete: 'false'
+      },
+      {
+        id: id(),
+        content: 'Update Self service code for route to SPOT and Livefyre',
+        complete: 'false'
+      },
+      {
+        id: id(),
+        content: 'Turn on SPOT for NGN',
+        complete: 'false'
+      },
+      {
+        id: id(),
+        content: 'Remove switches for livefyre in SF, CAPI and SS',
+        complete: 'false'
+      }],
+      showComplete : true
+    }
+  }
+
+  getState() {
+    return this.__state;
+  }
+
+  reduce(state, action) {
+    console.log("Reducing ...", state, action);
+    return state;
+  }
+}
+
+const tasksStore = new TasksStore(tasksDispatcher);
+
+tasksDispatcher.dispatch("TEST_DISPATCH");
+
